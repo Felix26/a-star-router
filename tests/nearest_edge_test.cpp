02935968e7ce9c2739684aaf9ecd2cdf4cbc14b0
Felix Miller <FelixMiller2005@outlook.de>
2025-11-10 23:26:26 +0100

    tests and tests

---
+++ tests/nearest_edge_test.cpp
@@ -0,0 +1,47 @@
+#include <cstdint>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <cassert>
+
+#include <unordered_dense.h>
+
+#include "graph.hpp"
+#include "library.hpp"
+#include "osmnode.hpp"
+#include "osmway.hpp"
+#include "coordinates.hpp"
+
+int main()
+{
+    try
+    {
+        Graph graph;
+        ankerl::unordered_dense::map<u_int64_t, std::shared_ptr<OsmNode>> nodes;
+        ankerl::unordered_dense::map<uint64_t, std::unique_ptr<OsmWay>> ways;
+
+        const std::string osmPath = std::string(PROJECT_SOURCE_DIR) + "/testdata/neureut.osm";
+        HelperFunctions::readOSMFile(osmPath, nodes, ways);
+        HelperFunctions::createGraph(graph, nodes, ways);
+
+        Coordinates testCoordinates = {49.053453, 8.384183};
+        uint64_t assertionEdgeId = 779542839;
+
+        uint64_t closestEdgeId = graph.getClosestEdgeId(testCoordinates) & 0x00FFFFFFFFFFFFFF;
+
+        assert(closestEdgeId == assertionEdgeId);
+        
+
+        testCoordinates = {49.050133, 8.390682};
+        assertionEdgeId = 132153778;
+
+        closestEdgeId = graph.getClosestEdgeId(testCoordinates) & 0x00FFFFFFFFFFFFFF;
+
+        assert(closestEdgeId == assertionEdgeId);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Fehler im Test: " << e.what() << "\n";
+        return 1;
+    }
+}
---
+++ tests/neureut_graph_test.cpp
@@ -0,0 +1,32 @@
+#include <cstdint>
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <unordered_dense.h>
+
+#include "graph.hpp"
+#include "library.hpp"
+#include "osmnode.hpp"
+#include "osmway.hpp"
+
+int main()
+{
+    try
+    {
+        Graph graph;
+        ankerl::unordered_dense::map<u_int64_t, std::shared_ptr<OsmNode>> nodes;
+        ankerl::unordered_dense::map<uint64_t, std::unique_ptr<OsmWay>> ways;
+
+        const std::string osmPath = std::string(PROJECT_SOURCE_DIR) + "/testdata/neureut.osm";
+        HelperFunctions::readOSMFile(osmPath, nodes, ways);
+        HelperFunctions::createGraph(graph, nodes, ways);
+
+        return graph.getNodes().empty() ? 1 : 0;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Fehler im Test: " << e.what() << "\n";
+        return 1;
+    }
+}
